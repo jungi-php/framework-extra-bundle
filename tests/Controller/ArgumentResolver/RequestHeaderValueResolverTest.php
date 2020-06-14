f86e44b58ae58628e1e7c323a1880a59c12cf361
Piotr Kugla <piku235@gmail.com>
2020-06-14 17:02:36 +0200

    Cover array argument in @RequestHeader tests

--- tests/Controller/ArgumentResolver/RequestHeaderValueResolverTest.php
+++ tests/Controller/ArgumentResolver/RequestHeaderValueResolverTest.php
@@ -2,12 +2,15 @@
 
 namespace Jungi\FrameworkExtraBundle\Tests\Controller\ArgumentResolver;
 
+use Jungi\FrameworkExtraBundle\Annotation\ClassMethodAnnotationRegistry;
 use Jungi\FrameworkExtraBundle\Annotation\RequestFieldAnnotationInterface;
 use Jungi\FrameworkExtraBundle\Annotation\RequestHeader;
 use Jungi\FrameworkExtraBundle\Controller\ArgumentResolver\RequestHeaderValueResolver;
 use Jungi\FrameworkExtraBundle\Converter\ConverterInterface;
+use Jungi\FrameworkExtraBundle\Http\RequestUtils;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
 /**
  * @author Piotr Kugla <piku235@gmail.com>
@@ -17,6 +20,27 @@ class RequestHeaderValueResolverTest extends AbstractRequestFieldValueResolverTe
     public function argumentTypeSameAsParameterType()
     {
         $this->markTestSkipped('always as string value');
+    }
+
+    /** @test */
+    public function argumentOfArrayType()
+    {
+        $converter = $this->createMock(ConverterInterface::class);
+        $converter
+            ->expects($this->never())
+            ->method('convert');
+
+        $resolver = $this->createArgumentValueResolver($converter);
+        $request = new Request();
+        $request->headers->set('foo', ['one', 'second']);
+
+        RequestUtils::setControllerAnnotationRegistry($request, new ClassMethodAnnotationRegistry([], [], [
+            new RequestHeader(array('argument' => 'foo', 'field' => 'foo')),
+        ]));
+
+        $argumentMetadata =  new ArgumentMetadata('foo', 'array', false, false, null);
+
+        $this->assertEquals(['one', 'second'], $resolver->resolve($request, $argumentMetadata)->current());
     }
 
     protected function createArgumentValueResolver(ConverterInterface $converter): ArgumentValueResolverInterface
