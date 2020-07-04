5ed7d27c8390a1dd1983583aa355a3ed7c2e5bcf
Piotr Kugla <piku235@gmail.com>
2020-07-04 12:23:51 +0200

    Detach AnnotationInterface from ExportableObject

--- src/Annotation/AnnotationInterface.php
+++ src/Annotation/AnnotationInterface.php
@@ -2,11 +2,9 @@
 
 namespace Jungi\FrameworkExtraBundle\Annotation;
 
-use Jungi\FrameworkExtraBundle\DependencyInjection\ExportableObject;
-
 /**
  * @author Piotr Kugla <piku235@gmail.com>
  */
-interface AnnotationInterface extends ExportableObject
+interface AnnotationInterface
 {
 }
--- src/Annotation/ResponseBody.php
+++ src/Annotation/ResponseBody.php
@@ -2,12 +2,14 @@
 
 namespace Jungi\FrameworkExtraBundle\Annotation;
 
+use Jungi\FrameworkExtraBundle\DependencyInjection\ExportableObject;
+
 /**
  * @author Piotr Kugla <piku235@gmail.com>
  *
  * @Annotation
  * @Target({"CLASS","METHOD"})
  */
-class ResponseBody implements AnnotationInterface
+class ResponseBody implements AnnotationInterface, ExportableObject
 {
 }
