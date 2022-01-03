a98bcb8a2b7fe323e4571dda6165eefc0124c6d9
Piotr Kugla <piku235@gmail.com>
2022-01-03 16:25:03 +0100

    Remove older Serializer version

--- src/Serializer/Serializer.php
+++ src/Serializer/Serializer.php
@@ -4,44 +4,22 @@ namespace Jungi\FrameworkExtraBundle\Serializer;
 
 use Symfony\Component\Serializer\Serializer as AbstractSerializer;
 
-if (!defined('Symfony\Component\Serializer\Encoder\XmlEncoder::TYPE_CASE_ATTRIBUTES')) { // removed in v5.0, method signature updated
-    /**
-     * @author Piotr Kugla <piku235@gmail.com>
-     */
-    final class Serializer extends AbstractSerializer
+/**
+ * @author Piotr Kugla <piku235@gmail.com>
+ */
+final class Serializer extends AbstractSerializer
+{
+    public function denormalize($data, string $type, string $format = null, array $context = [])
     {
-        public function denormalize($data, string $type, string $format = null, array $context = [])
-        {
-            if ($data instanceof $type) {
-                return $data;
-            }
-
-            return parent::denormalize($data, $type, $format, $context);
+        if ($data instanceof $type) {
+            return $data;
         }
 
-        public function supportsDenormalization($data, string $type, string $format = null, array $context = [])
-        {
-            return $data instanceof $type || parent::supportsDenormalization($data, $type, $format, $context);
-        }
+        return parent::denormalize($data, $type, $format, $context);
     }
-} else {
-    /**
-     * @author Piotr Kugla <piku235@gmail.com>
-     */
-    final class Serializer extends AbstractSerializer
+
+    public function supportsDenormalization($data, string $type, string $format = null, array $context = [])
     {
-        public function denormalize($data, $type, $format = null, array $context = [])
-        {
-            if ($data instanceof $type) {
-                return $data;
-            }
-
-            return parent::denormalize($data, $type, $format, $context);
-        }
-
-        public function supportsDenormalization($data, $type, $format = null, array $context = [])
-        {
-            return $data instanceof $type || parent::supportsDenormalization($data, $type, $format, $context);
-        }
+        return $data instanceof $type || parent::supportsDenormalization($data, $type, $format, $context);
     }
 }
