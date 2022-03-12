dc1976a9860a5c7fbfbcd76f0c060d94c0b78464
Piotr Kugla <piku235@gmail.com>
2022-03-12 18:39:13 +0100

    Adjust BuiltinTypeSafeConverter to PHP 8

--- src/Converter/BuiltinTypeSafeConverter.php
+++ src/Converter/BuiltinTypeSafeConverter.php
@@ -12,24 +12,13 @@ final class BuiltinTypeSafeConverter implements ConverterInterface
     public function convert(mixed $value, string $type): string|int|bool|float
     {
         try {
-            switch ($type) {
-                case 'int':
-                    $result = @$this->convertToInt($value);
-                    $this->handleNonNumericValueNotice();
-
-                    return $result;
-                case 'float':
-                    $result = @$this->convertToFloat($value);
-                    $this->handleNonNumericValueNotice();
-
-                    return $result;
-                case 'bool':
-                    return $this->convertToBool($value);
-                case 'string':
-                    return $this->convertToString($value);
-                default:
-                    throw new \InvalidArgumentException(sprintf('Unsupported type "%s".', $type));
-            }
+            return match ($type) {
+                'int' => $this->convertToInt($value),
+                'float' => $this->convertToFloat($value),
+                'bool' => $this->convertToBool($value),
+                'string' => $this->convertToString($value),
+                default => throw new \InvalidArgumentException(sprintf('Unsupported type "%s".', $type)),
+            };
         } catch (\TypeError $e) {
             throw new TypeConversionException($e->getMessage(), 0, $e);
         }
@@ -53,15 +42,5 @@ final class BuiltinTypeSafeConverter implements ConverterInterface
     private function convertToString(string $value): string
     {
         return $value;
-    }
-
-    private function handleNonNumericValueNotice(): void
-    {
-        $error = error_get_last();
-        if ($error && 'A non well formed numeric value encountered' === $error['message']) {
-            error_clear_last();
-
-            throw new TypeConversionException($error['message']);
-        }
     }
 }
